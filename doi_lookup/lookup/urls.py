9eb0d479e1c5c903876c5da9d35b6761f5f62a26
Joseph Muller <jhmuller@umich.edu>
2021-07-10 20:12:30 -0400

    make list view and write basic css

--- doi_lookup/lookup/urls.py
+++ doi_lookup/lookup/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.index, name = 'index'),
+    path('articles/', views.ArticleListView.as_view(), name = 'articles')
 ]
--- doi_lookup/lookup/views.py
+++ doi_lookup/lookup/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from .models import Article
+
+def index(request):
+    """View for landing page with DOI lookup form"""
+    context = {}
+
+    return render(request, 'index.html', context = context)
+
+class ArticleListView(generic.ListView):
+    model = Article
